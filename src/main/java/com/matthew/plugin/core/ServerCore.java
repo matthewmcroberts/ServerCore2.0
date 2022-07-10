d5fb2c890d56afb45b7f9fd8cf3b93c74e06e36d
GoofIt <mm20captingoof@gmail.com>
2022-07-10 18:54:53 -0400

    adding core to rep

---
+++ src/main/java/com/matthew/plugin/core/ServerCore.java
@@ -0,0 +1,17 @@
+package com.matthew.plugin.core;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ServerCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
