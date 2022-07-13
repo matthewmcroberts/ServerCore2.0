2dabf92f12ad2170d454abefae4889d4258437d0
GoofIt <mm20captingoof@gmail.com>
2022-07-12 22:59:09 -0400

    added check if sender is a player

--- src/main/java/com/matthew/plugin/core/commands/inventory/commands/InventoryCommand.java
+++ src/main/java/com/matthew/plugin/core/commands/inventory/commands/InventoryCommand.java
@@ -19,48 +19,51 @@ public class InventoryCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
-        Player player = (Player) sender;
 
         try {
-            if (RankUtils.isAdmin(player)) {
-                if (args.length == 2) {
-                    switch (args[0]) {
-                        case "clear":
-                            Player target = Bukkit.getPlayerExact(args[1]);
-                            if (target != null) {
-                                InventoryManager.inventoryClear(player, target);
-                            } else {
-                                MessageUtils.playerNotFound(player);
-                            }
+            if(sender instanceof Player) {
+                Player player = (Player) sender;
 
-                            break;
-                        case "restore":
-                            Player target2 = Bukkit.getPlayerExact(args[1]);
-                            if (target2 != null) {
-                                InventoryManager.inventoryRestore(player, target2);
-                            } else {
-                                MessageUtils.playerNotFound(player);
-                            }
+                if (RankUtils.isAdmin(player)) {
+                    if (args.length == 2) {
+                        switch (args[0]) {
+                            case "clear":
+                                Player target = Bukkit.getPlayerExact(args[1]);
+                                if (target != null) {
+                                    InventoryManager.inventoryClear(player, target);
+                                } else {
+                                    MessageUtils.playerNotFound(player);
+                                }
 
-                            break;
-                        default:
-                            MessageUtils.incorrectUsage(player, "/inventory clear");
-                            player.sendMessage(ChatColor.BLUE + "    - " + ChatColor.GOLD + "/inventory clear (player)");
-                            player.sendMessage(ChatColor.BLUE + "    - " + ChatColor.GOLD + "/inventory restore (player)");
+                                break;
+                            case "restore":
+                                Player target2 = Bukkit.getPlayerExact(args[1]);
+                                if (target2 != null) {
+                                    InventoryManager.inventoryRestore(player, target2);
+                                } else {
+                                    MessageUtils.playerNotFound(player);
+                                }
 
-                            break;
+                                break;
+                            default:
+                                MessageUtils.incorrectUsage(player, "/inventory clear");
+                                player.sendMessage(ChatColor.BLUE + "    - " + ChatColor.GOLD + "/inventory clear (player)");
+                                player.sendMessage(ChatColor.BLUE + "    - " + ChatColor.GOLD + "/inventory restore (player)");
+
+                                break;
+                        }
+
+                    } else if ((args.length == 1) && args[0].equalsIgnoreCase("clear")) {
+                        InventoryManager.inventoryClear(player);
+
+                    } else {
+                        MessageUtils.incorrectUsage(player, "/inventory clear");
+                        player.sendMessage(ChatColor.BLUE + "    - " + ChatColor.GOLD + "/inventory clear (player)");
+                        player.sendMessage(ChatColor.BLUE + "    - " + ChatColor.GOLD + "/inventory restore (player)");
                     }
-
-                } else if((args.length == 1) && args[0].equalsIgnoreCase("clear")) {
-                    InventoryManager.inventoryClear(player);
-
                 } else {
-                    MessageUtils.incorrectUsage(player, "/inventory clear");
-                    player.sendMessage(ChatColor.BLUE + "    - " + ChatColor.GOLD + "/inventory clear (player)");
-                    player.sendMessage(ChatColor.BLUE + "    - " + ChatColor.GOLD + "/inventory restore (player)");
+                    MessageUtils.adminRank(player);
                 }
-            } else {
-                MessageUtils.adminRank(player);
             }
         } catch (SQLException e) {
             e.printStackTrace();
