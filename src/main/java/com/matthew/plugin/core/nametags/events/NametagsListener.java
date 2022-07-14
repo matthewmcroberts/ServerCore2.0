083d03a4d3d318eab8056b97e9829765689a68d9
GoofIt <mm20captingoof@gmail.com>
2022-07-13 22:45:39 -0400

    removing onPlayerQuit event

--- src/main/java/com/matthew/plugin/core/nametags/events/NametagsListener.java
+++ src/main/java/com/matthew/plugin/core/nametags/events/NametagsListener.java
@@ -7,7 +7,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
 
 import java.sql.SQLException;
 
@@ -15,64 +14,6 @@ public class NametagsListener implements Listener {
 
     @EventHandler
     public void onPlayerJoinEvent(PlayerJoinEvent e) throws SQLException {
-        Player player = e.getPlayer();
-
-        player.setScoreboard(NametagsManager.tabBoard);
-        switch (RankManager.getRank(player).getName()) {
-            case "OWNER":
-
-                NametagsUtils.addToOwnerTeam(player);
-                break;
-            case "DEV":
-
-                NametagsUtils.addToDevTeam(player);
-                break;
-            case "QAT":
-
-                NametagsUtils.addToQatTeam(player);
-                break;
-            case "ADMIN":
-
-                NametagsUtils.addToAdminTeam(player);
-                break;
-            case "SRMOD":
-
-                NametagsUtils.addToSrModTeam(player);
-                break;
-            case "MOD":
-
-                NametagsUtils.addToModTeam(player);
-                break;
-            case "JRMOD":
-
-                NametagsUtils.addToJrModTeam(player);
-                break;
-            case "BUILDER":
-
-                NametagsUtils.addToBuilderTeam(player);
-                break;
-            case "GOD":
-
-                NametagsUtils.addToGodTeam(player);
-                break;
-            case "SLAYER":
-
-                NametagsUtils.addToSlayerTeam(player);
-                break;
-            case "KNOWN":
-
-                NametagsUtils.addToKnownTeam(player);
-                break;
-            case "MEMBER":
-
-                NametagsUtils.addToTeamMember(player);
-                break;
-
-        }
-    }
-
-    @EventHandler
-    public void onPlayerQuitEvent(PlayerQuitEvent e) throws SQLException {
         Player player = e.getPlayer();
 
         player.setScoreboard(NametagsManager.tabBoard);
