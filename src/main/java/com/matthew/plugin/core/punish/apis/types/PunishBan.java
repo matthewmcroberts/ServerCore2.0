ced92ec82bc10cce385ad48ff396ec93b3306ef0
Matthew <matthew.mcroberts22@gmail.com>
2023-08-21 17:47:46 -0400

    commenting out error

--- src/main/java/com/matthew/plugin/core/punish/apis/types/PunishBan.java
+++ src/main/java/com/matthew/plugin/core/punish/apis/types/PunishBan.java
@@ -11,24 +11,23 @@ import java.util.Calendar;
 
 public class PunishBan {
 
-    public static void permBan(Player sender, OfflinePlayer player, String reason) {
-        Bukkit.getBanList(BanList.Type.NAME).addBan(player.getName(), ChatColor.RED.toString() + ChatColor.BOLD +"You are banned for Permanent\n" + reason, null, null);
-        MessageUtils.sendCustomMessage(sender, "Successfully banned " + ChatColor.YELLOW + player.getName() + ChatColor.GRAY + " for: ");
-        MessageUtils.addToList(sender, ChatColor.GRAY + reason);
-        MessageUtils.addToList(sender, ChatColor.GRAY + "Until: " + ChatColor.GOLD + "Permanent");
-        if(player.isOnline()) {
-            player.getPlayer().kickPlayer(ChatColor.RED.toString() + ChatColor.BOLD + "You have been banned for " + reason);
-        }
+    // ID INTEGER(PRIMARY)(AI)
+    // uuid VARCHAR
+    // type VARCHAR
+    // sev INTEGER
+    // reason TINYTEXT
+    // issued TIMESTAMP
+    // expiration TIMESTAMP
+    // staff VARCHAR
+    // active BOOLEAN
+    // permanent BOOLEAN
+
+    public static void permBan(Player issuer, OfflinePlayer target, String type, String reason, boolean permanent) {
+
     }
 
     public static void tempBan(Player sender, OfflinePlayer player, Calendar time, String reason) {
-        Bukkit.getBanList(BanList.Type.NAME).addBan(player.getName(), ChatColor.RED.toString() + ChatColor.BOLD +"You are banned for\n" + reason, time.getTime(), null);
-        MessageUtils.sendCustomMessage(sender, "Successfully banned " + ChatColor.YELLOW + player.getName() + ChatColor.GRAY + " for: ");
-        MessageUtils.addToList(sender, ChatColor.GRAY + reason);
-        MessageUtils.addToList(sender, ChatColor.GRAY + "Until: " + ChatColor.GOLD + time.getTime());
-        if(player.isOnline()) {
-            player.getPlayer().kickPlayer(ChatColor.RED.toString() + ChatColor.BOLD + "You have been banned for " + reason);
-        }
+
     }
 
 }
