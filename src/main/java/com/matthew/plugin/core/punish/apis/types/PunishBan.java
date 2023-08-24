8c2e35127f2811a9a28542fe77e04ab33580d29c
Matthew <matthew.mcroberts22@gmail.com>
2023-08-23 23:01:47 -0400

    finished logic

--- src/main/java/com/matthew/plugin/core/punish/apis/types/PunishBan.java
+++ src/main/java/com/matthew/plugin/core/punish/apis/types/PunishBan.java
@@ -9,8 +9,10 @@ import org.bukkit.ChatColor;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 
+import javax.annotation.Nullable;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.util.Calendar;
 
 public class PunishBan {
@@ -28,28 +30,45 @@ public class PunishBan {
 
     public static void permBan(Player issuer, OfflinePlayer target, String type, String reason, boolean permanent) throws SQLException {
         if(ActivePunishments.getAmount(target) == 0) {
-
-
+            insert(issuer, target, type, 4, reason, new Timestamp(System.currentTimeMillis()), permanent);
         } else if(ActivePunishments.getAmount(target) == 1) {
-            //check if punishment is chat
-            //issue ban
-            //if ban, deny ability to ban
+            if(ActivePunishments.getType(target).get(0).equals("Chat")) {
+                insert(issuer, target, type, 4, reason, new Timestamp(System.currentTimeMillis()), permanent);
+            } else {
+                MessageUtils.sendCustomMessage(issuer, ChatColor.GOLD + target.getName() + ChatColor.GRAY + "is already banned.");
+            }
         } else {
-            //say player is already banned
+            MessageUtils.sendCustomMessage(issuer, ChatColor.GOLD + target.getName() + ChatColor.GRAY + "is already banned.");
         }
     }
 
-    public static void tempBan(Player sender, OfflinePlayer player, Calendar time, String reason) {
-
+    public static void tempBan(Player issuer, OfflinePlayer target, String type, String reason, Timestamp expiration, boolean permanent) throws SQLException {
+        if(ActivePunishments.getAmount(target) == 0) {
+            insert(issuer, target, type, 4, reason, expiration, permanent);
+        } else if(ActivePunishments.getAmount(target) == 1) {
+            if(ActivePunishments.getType(target).get(0).equals("Chat")) {
+                insert(issuer, target, type, 4, reason, expiration, permanent);
+            } else {
+                MessageUtils.sendCustomMessage(issuer, ChatColor.GOLD + target.getName() + ChatColor.GRAY + "is already banned.");
+            }
+        } else {
+            MessageUtils.sendCustomMessage(issuer, ChatColor.GOLD + target.getName() + ChatColor.GRAY + "is already banned.");
+        }
     }
 
-    private static void insert(Player issuer, OfflinePlayer target, String type, String reason, boolean permanent) throws SQLException {
+    private static void insert(Player issuer, OfflinePlayer target, String type, int sev, String reason, Timestamp expiration, boolean permanent) throws SQLException {
         PreparedStatement ps = ServerCore.preparedStatement("INSERT INTO active_punishments(ID, UUID, TYPE, SEV, REASON, ISSUED, EXPIRATION, STAFF, ACTIVE, PERMANENT) VALUES(" +
-                "DEFAULT, ?, ?, ?, ?, DEFAULT, ?, ?, DEFAULT, 1);");
+                "DEFAULT, ?, ?, ?, ?, DEFAULT, ?, ?, DEFAULT, ?);");
         ps.setString(1, target.getUniqueId().toString());
         ps.setString(2, type);
-        ps.setString(3, reason);
-        ps.setString(4, "DEFAULT");
+        ps.setInt(3, sev);
+        ps.setString(4, reason);
+        ps.setTimestamp(5, expiration);
+        ps.setString(6, issuer.getUniqueId().toString());
+        ps.setBoolean(7, permanent);
+
+        ps.execute();
+
     }
 
 }
