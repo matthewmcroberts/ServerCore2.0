2a16bac5328da867ea2ff9b49b643c4aa7b162d6
Matthew <matthew.mcroberts22@gmail.com>
2023-08-24 17:41:09 -0400

    updated to implement new apis and logic

--- src/main/java/com/matthew/plugin/core/punish/events/PunishListener.java
+++ src/main/java/com/matthew/plugin/core/punish/events/PunishListener.java
@@ -2,6 +2,7 @@ package com.matthew.plugin.core.punish.events;
 
 import com.matthew.plugin.core.ServerCore;
 import com.matthew.plugin.core.punish.apis.types.PunishBan;
+import com.matthew.plugin.core.punish.apis.types.PunishMute;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.OfflinePlayer;
@@ -11,13 +12,14 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 
+import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.util.Calendar;
 
 public class PunishListener implements Listener {
 
-    //@SuppressWarnings("DuplicateBranchesInSwitch")
-    /*@EventHandler
-    public void onClick(InventoryClickEvent e) {
+    @EventHandler
+    public void onClick(InventoryClickEvent e) throws SQLException {
 
         Player player = (Player) e.getWhoClicked();
         Calendar cal = Calendar.getInstance();
@@ -29,71 +31,87 @@ public class PunishListener implements Listener {
 
                 switch (e.getRawSlot()) {
                     case 20: //mute sev 1
+                        Timestamp time = new Timestamp(System.currentTimeMillis());
+                        time.setTime((long) (time.getTime() + 8.64e7)); //1 Day
                         player.closeInventory();
+                        PunishMute.tempMute(player, target, 1, ServerCore.punishReason.get(player), time);
                         ServerCore.punishReason.remove(player);
 
                         break;
                     case 29: //mute sev 2
+                        time = new Timestamp(System.currentTimeMillis());
+                        time.setTime((long) (time.getTime() + 6.048e8)); //7 Days
                         player.closeInventory();
+                        PunishMute.tempMute(player, target, 2, ServerCore.punishReason.get(player), time);
                         ServerCore.punishReason.remove(player);
 
                         break;
                     case 38: //mute sev 3
+                        time = new Timestamp(System.currentTimeMillis());
+                        time.setTime((long) (time.getTime() + 2.592e9)); //30 Days
                         player.closeInventory();
+                        PunishMute.tempMute(player, target, 3, ServerCore.punishReason.get(player), time);
                         ServerCore.punishReason.remove(player);
 
                         break;
                     case 22: //gameplay ban sev 1
-                        cal.add(Calendar.HOUR, 1);
-
-                        PunishBan.tempBan(player, target, cal, ServerCore.punishReason.get(player));
+                        time = new Timestamp(System.currentTimeMillis());
+                        time.setTime((long) (time.getTime() + 3.6e6)); // 1hr
                         player.closeInventory();
+                        PunishBan.tempBan(player, target, "Gameplay", 1, ServerCore.punishReason.get(player), time);
                         ServerCore.punishReason.remove(player);
 
                         break;
                     case 31: //gameplay ban sev 2
-                    case 24: //hacking ban sev 1
-                        cal.add(Calendar.HOUR, 12);
-
-                        PunishBan.tempBan(player, target, cal, ServerCore.punishReason.get(player));
+                        time = new Timestamp(System.currentTimeMillis());
+                        time.setTime((long) (time.getTime() + 8.64e7)); //1 Day
                         player.closeInventory();
+                        PunishBan.tempBan(player, target, "Gameplay", 2, ServerCore.punishReason.get(player), time);
                         ServerCore.punishReason.remove(player);
 
                         break;
-                    case 40: //gameplay ban sev 3
-                        cal.add(Calendar.DAY_OF_WEEK, 1);
-
-                        PunishBan.tempBan(player, target, cal, ServerCore.punishReason.get(player));
+                    case 24: //gameplay ban sev 3
+                        time = new Timestamp(System.currentTimeMillis());
+                        time.setTime((long) (time.getTime() + 6.048e8)); //7 Days
                         player.closeInventory();
+                        PunishBan.tempBan(player, target, "Gameplay", 3, ServerCore.punishReason.get(player), time);
+                        ServerCore.punishReason.remove(player);
+
+                        break;
+                    case 40: //hacking ban sev 1
+                        time = new Timestamp(System.currentTimeMillis());
+                        time.setTime((long) (time.getTime() + 8.64e7)); //1 Day
+                        player.closeInventory();
+                        PunishBan.tempBan(player, target, "Hacking", 3, ServerCore.punishReason.get(player), time);
                         ServerCore.punishReason.remove(player);
 
                         break;
 
-                    case 33: //hacking ban sev 2
-                        cal.add(Calendar.DAY_OF_WEEK, 7);
-
-                        PunishBan.tempBan(player, target, cal, ServerCore.punishReason.get(player));
+                    case 33: //hacking b an sev 2
+                        time = new Timestamp(System.currentTimeMillis());
+                        time.setTime((long) (time.getTime() + 6.048e8)); //7 Days
                         player.closeInventory();
-                        player.closeInventory();
+                        PunishBan.tempBan(player, target, "Hacking", 3, ServerCore.punishReason.get(player), time);
                         ServerCore.punishReason.remove(player);
 
                         break;
                     case 42: //hacking ban sev 3
-                        cal.add(Calendar.MONTH, 1);
-
-                        PunishBan.tempBan(player, target, cal, ServerCore.punishReason.get(player));
+                        time = new Timestamp(System.currentTimeMillis());
+                        time.setTime((long) (time.getTime() + 2.592e9)); //30 Days
                         player.closeInventory();
+                        PunishBan.tempBan(player, target, "Hacking", 3, ServerCore.punishReason.get(player), time);
                         ServerCore.punishReason.remove(player);
 
                         break;
                     case 27: //perm mute
                         player.closeInventory();
+                        PunishMute.permMute(player, target, ServerCore.punishReason.get(player));
                         ServerCore.punishReason.remove(player);
 
                         break;
                     case 36: //perm ban
-                        //PunishBan.permBan(player, target, ServerCore.punishReason.get(player));
                         player.closeInventory();
+                        PunishBan.permBan(player, target, "Gameplay", ServerCore.punishReason.get(player));
                         ServerCore.punishReason.remove(player);
 
                         break;
@@ -110,7 +128,7 @@ public class PunishListener implements Listener {
             ServerCore.punishReason.remove(e.getPlayer());
         }
     }
-
+/*
     @EventHandler
     public void onJoin(PlayerJoinEvent e) {
         Player player = e.getPlayer();
