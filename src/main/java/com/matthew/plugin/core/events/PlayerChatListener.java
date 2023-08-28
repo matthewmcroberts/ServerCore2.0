c2d9d5887fd0dcf81f5508ebec378328422a2f66
Matthew <matthew.mcroberts22@gmail.com>
2023-08-27 21:01:48 -0400

    stopped muted players from chatting

--- src/main/java/com/matthew/plugin/core/events/PlayerChatListener.java
+++ src/main/java/com/matthew/plugin/core/events/PlayerChatListener.java
@@ -1,6 +1,7 @@
 package com.matthew.plugin.core.events;
 
 import com.matthew.plugin.core.commands.silence.SilenceManager;
+import com.matthew.plugin.core.punish.apis.dbquerys.ActivePunishments;
 import com.matthew.plugin.core.ranks.Ranks;
 import com.matthew.plugin.core.ranks.apis.RankManager;
 import com.matthew.plugin.core.utils.RankUtils;
@@ -25,29 +26,33 @@ public class PlayerChatListener implements Listener {
         Player player = e.getPlayer();
 
         Ranks rank = RankManager.getRank(player);
-
-        if(RankManager.getRank(player).equals(Ranks.SRMOD)) {
-            for (Player onlinePlayers : e.getRecipients()) {
-                onlinePlayers.sendMessage(rank.getColor() + ChatColor.BOLD.toString() + "SR.MOD " + rank.getColor() + player.getName() + " " + ChatColor.WHITE + e.getMessage());
-            }
-            e.setCancelled(true);
-        } else if(RankManager.getRank(player).equals(Ranks.JRMOD)) {
-            for (Player onlinePlayers : e.getRecipients()) {
-                onlinePlayers.sendMessage(rank.getColor() + ChatColor.BOLD.toString() + "JR.MOD " + rank.getColor() + player.getName() + " " + ChatColor.WHITE + e.getMessage());
-            }
-            e.setCancelled(true);
-        } else {
-            if(RankUtils.isNotStaff(player)) {
-                if(!SilenceManager.getSilenceManager().contains(player)) {
+        if(!ActivePunishments.isMuted(player)) {
+            if (RankManager.getRank(player).equals(Ranks.SRMOD)) {
+                for (Player onlinePlayers : e.getRecipients()) {
+                    onlinePlayers.sendMessage(rank.getColor() + ChatColor.BOLD.toString() + "SR.MOD " + rank.getColor() + player.getName() + " " + ChatColor.WHITE + e.getMessage());
+                }
+                e.setCancelled(true);
+            } else if (RankManager.getRank(player).equals(Ranks.JRMOD)) {
+                for (Player onlinePlayers : e.getRecipients()) {
+                    onlinePlayers.sendMessage(rank.getColor() + ChatColor.BOLD.toString() + "JR.MOD " + rank.getColor() + player.getName() + " " + ChatColor.WHITE + e.getMessage());
+                }
+                e.setCancelled(true);
+            } else {
+                if (RankUtils.isNotStaff(player)) {
+                    if (!SilenceManager.getSilenceManager().contains(player)) {
+                        for (Player onlinePlayers : e.getRecipients()) {
+                            onlinePlayers.sendMessage(rank.getColor() + ChatColor.BOLD.toString() + rank.getName() + " " + rank.getColor() + player.getName() + " " + ChatColor.WHITE + e.getMessage());
+                        }
+                    }
+                } else {
                     for (Player onlinePlayers : e.getRecipients()) {
                         onlinePlayers.sendMessage(rank.getColor() + ChatColor.BOLD.toString() + rank.getName() + " " + rank.getColor() + player.getName() + " " + ChatColor.WHITE + e.getMessage());
                     }
                 }
-            } else {
-                for (Player onlinePlayers : e.getRecipients()) {
-                    onlinePlayers.sendMessage(rank.getColor() + ChatColor.BOLD.toString() + rank.getName() + " " + rank.getColor() + player.getName() + " " + ChatColor.WHITE + e.getMessage());
-                }
+                e.setCancelled(true);
             }
+        } else {
+            player.sendMessage("You are muted");
             e.setCancelled(true);
         }
     }
